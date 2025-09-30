c4f4cef05be526303c252e4ab8cc3572e27db543
raghuveersharma <sharmaraghu157@gmail.com>
2025-09-30 18:08:04 +0530

    add field for blog and email,password.

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,5 @@
-import { Document, model, Schema } from "mongoose";
+import mongoose, { Document, model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 export enum UserRole {
   ADULT = "adult",
   TEEN = "teen",
@@ -10,6 +11,9 @@ interface IUser {
   lastName: String;
   age: Number;
   status?: UserRole;
+  Blogs: mongoose.Types.ObjectId;
+  email: String;
+  password: string;
 }
 interface UserDocument extends IUser, Document {
   fullName: String;
@@ -26,10 +30,20 @@ const userSchema = new Schema<UserDocument>(
       type: Number,
       required: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     status: {
       type: String,
       enum: Object.values(UserRole),
     },
+    Blogs: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
   {
     timestamps: true,
@@ -48,16 +62,18 @@ userSchema.virtual("fullName").get(function (this: UserDocument) {
   return `${this.firstName} ${this.lastName}`;
 });
 
-userSchema.pre("save", function (next) {
-  if (this.isModified("firstName") || this.isModified("lastName")) {
-    console.log("Full name changed");
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
   }
+  const salt = await bcrypt.genSalt(10);
+  const hash = bcrypt.hash(this.password, salt);
+  this.password = await hash;
   next();
 });
 
-userSchema.methods.pp = function () {
-  console.log("hello");
-  return 44;
+userSchema.methods.matchPassword = async function () {
+  return await bcrypt.compare(this.password, this.password);
 };
 
 const User = model("User", userSchema);
